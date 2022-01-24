3bd507775d1d3d47014fbc733f724855a100b133
Iván Alexis <ivanchoa96@gmail.com>
2022-01-24 16:28:02 -0500

    Realize una indicacion en la linea de PIN

---
+++ Primer codigo en nuestro repositorio.cpp
@@ -0,0 +1,13 @@
+//Primer codigo en nuestro repositorio
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
